a0a2bed21777a53aa1f531593d0e6493a0014cf5
karel-rehor <karel.k.rehor@email.cz>
2019-02-11 14:14:38 +0100

    additional flux system tests

--- src/test/java/com/bonitoo/qa/dashboards/SystemDashTest.java
+++ src/test/java/com/bonitoo/qa/dashboards/SystemDashTest.java
@@ -173,8 +173,6 @@ public class SystemDashTest {
                 TestRunner.getTestConf().getOrg().getBucket(),
                 TestRunner.getTestConf().getHostname());
 
-        System.out.println("DEBUG query: " + query);
-
         List<FluxTable> tables = queryClient.query(query, TestRunner.getInflux2conf().getOrgId());
 
         assertThat(tables.size()).isGreaterThan(0);
@@ -197,6 +195,254 @@ public class SystemDashTest {
 
     @Test
     public void DiskIOTest(){
+
+        String query = String.format("dashboardTime = -15m\n" +
+                "from(bucket: \"%s\")\n" +
+                "  |> range(start: dashboardTime)\n" +
+                "  |> filter(fn: (r) => r._measurement == \"diskio\" and r.host==\"%s\" and (r._field == \"read_bytes\" or r._field == \"write_bytes\"))\n" +
+                "  |> derivative(unit: 1s, nonNegative: true, columns: [\"_value\"])\n" +
+                "  |> group(columns: [\"_field\",\"name\"], mode: \"by\")",
+                TestRunner.getTestConf().getOrg().getBucket(),
+                TestRunner.getTestConf().getHostname());
+
+        List<FluxTable> tables = queryClient.query(query, TestRunner.getInflux2conf().getOrgId());
+
+        assertThat(tables.size()).isGreaterThan(0);
+
+        TestRunner.printTables(query, tables);
+
+        tables.forEach(table -> {
+            assertThat(table.getRecords().size()).isGreaterThan(0);
+            table.getRecords().forEach(record -> {
+                assertThat(record.getMeasurement()).isEqualTo("diskio");
+                assertThat(record.getField().equals("read_bytes") ||
+                        record.getField().equals("write_bytes")).isTrue();
+                assertThat(record.getValue() instanceof Double).isTrue();
+                assertThat((Double) record.getValue()).isGreaterThanOrEqualTo(0);
+            });
+        });
+
+    }
+
+    @Test
+    public void ProcessesTest(){
+
+        String query = String.format("dashboardTime = -15m\n" +
+                "autoInterval = 30s\n\n" +
+                "from(bucket: \"%s\")\n" +
+                "  |> range(start: dashboardTime)\n" +
+                "  |> filter(fn: (r) => r._measurement == \"processes\" and r.host == \"%s\" and r._field == \"total\")\n" +
+                "  |> window(every: autoInterval)\n" +
+                "  |> group(columns: [\"_field\"], mode: \"by\")",
+                TestRunner.getTestConf().getOrg().getBucket(),
+                TestRunner.getTestConf().getHostname());
+
+        List<FluxTable> tables = queryClient.query(query, TestRunner.getInflux2conf().getOrgId());
+
+        assertThat(tables.size()).isGreaterThan(0);
+
+        TestRunner.printTables(query, tables);
+
+        tables.forEach(table -> {
+            assertThat(table.getRecords().size()).isGreaterThan(0);
+            table.getRecords().forEach(record -> {
+                assertThat(record.getMeasurement()).isEqualTo("processes");
+                assertThat(record.getField()).isEqualTo("total");
+                assertThat(record.getValue() instanceof Long).isTrue();
+                assertThat((Long) record.getValue()).isGreaterThanOrEqualTo(0);
+            });
+        });
+
+    }
+
+    @Test
+    public void SwapTest(){
+
+        String query = String.format("dashboardTime = -15m\n" +
+                "autoInterval = 30s\n\n" +
+                "from(bucket: \"%s\")\n" +
+                "  |> range(start: dashboardTime)\n" +
+                "  |> filter(fn: (r) => r._measurement == \"swap\" and r.host == \"%s\" and (r._field == \"used\" or r._field == \"total\"))\n" +
+                "  |> window(every: autoInterval)\n" +
+                "  |> group(columns: [\"_field\"], mode: \"by\")",
+                TestRunner.getTestConf().getOrg().getBucket(),
+                TestRunner.getTestConf().getHostname());
+
+        List<FluxTable> tables = queryClient.query(query, TestRunner.getInflux2conf().getOrgId());
+
+        assertThat(tables.size()).isGreaterThan(0);
+
+        TestRunner.printTables(query, tables);
+
+        tables.forEach(table -> {
+            assertThat(table.getRecords().size()).isGreaterThan(0);
+            table.getRecords().forEach(record -> {
+                assertThat(record.getMeasurement()).isEqualTo("swap");
+                assertThat(record.getField().equals("total") ||
+                        record.getField().equals("used")).isTrue();
+                assertThat(record.getValue() instanceof Long).isTrue();
+                assertThat((Long) record.getValue()).isGreaterThanOrEqualTo(0);
+            });
+        });
+
+    }
+
+    @Test
+    public void MemoryUsage(){
+
+        String query = String.format("dashboardTime = -15m\n" +
+                "autoInterval = 30s\n\n" +
+                "from(bucket: \"%s\")\n" +
+                "  |> range(start: dashboardTime)\n" +
+                "  |> filter(fn: (r) => r._measurement == \"mem\" and r.host == \"%s\" and (r._field == \"used_percent\"))\n" +
+                "  |> window(every: autoInterval)\n" +
+                "  |> group(columns: [\"_field\"], mode: \"by\")\n",
+                TestRunner.getTestConf().getOrg().getBucket(),
+                TestRunner.getTestConf().getHostname());
+
+        List<FluxTable> tables = queryClient.query(query, TestRunner.getInflux2conf().getOrgId());
+
+        assertThat(tables.size()).isGreaterThan(0);
+
+        TestRunner.printTables(query, tables);
+
+        tables.forEach(table -> {
+            assertThat(table.getRecords().size()).isGreaterThan(0);
+            table.getRecords().forEach(record -> {
+                assertThat(record.getMeasurement()).isEqualTo("mem");
+                assertThat(record.getField()).isEqualTo("used_percent");
+                assertThat(record.getValue() instanceof Double).isTrue();
+                assertThat((Double) record.getValue()).isGreaterThanOrEqualTo(0);
+            });
+        });
+
+    }
+
+    @Test
+    public void FilesystemsUsage(){
+
+        String query = String.format("dashboardTime = -15m\n" +
+                "autoInterval = 30s\n\n" +
+                "from(bucket: \"%s\")\n" +
+                "  |> range(start: dashboardTime)\n" +
+                "  |> filter(fn: (r) => r._measurement == \"disk\" and (r._field == \"used_percent\") and r.host == \"%s\" )\n" +
+                "  |> window(every: autoInterval)\n" +
+                "  |> mean()\n" +
+                "  |> group(columns: [\"_field\",\"path\"], mode: \"by\")",
+                TestRunner.getTestConf().getOrg().getBucket(),
+                TestRunner.getTestConf().getHostname());
+
+        System.out.println("DEBUG query: " + query);
+
+        List<FluxTable> tables = queryClient.query(query, TestRunner.getInflux2conf().getOrgId());
+
+        assertThat(tables.size()).isGreaterThan(0);
+
+        TestRunner.printTables(query, tables);
+
+        tables.forEach(table -> {
+            assertThat(table.getRecords().size()).isGreaterThan(0);
+            table.getRecords().forEach(record -> {
+                assertThat(record.getMeasurement()).isEqualTo("disk");
+                assertThat(record.getField()).isEqualTo("used_percent");
+                assertThat(record.getValue() instanceof Double).isTrue();
+                assertThat((Double) record.getValue()).isGreaterThanOrEqualTo(0);
+            });
+        });
+
+    }
+
+    @Test
+    public void SystemLoad1Test(){
+
+        String query = String.format("dashboardTime = -15m\n" +
+                "from(bucket: \"%s\")\n" +
+                "  |> range(start: dashboardTime)\n" +
+                "  |> filter(fn: (r) => r._measurement == \"system\" and (r._field == \"load1\"))",
+                TestRunner.getTestConf().getOrg().getBucket());
+
+        List<FluxTable> tables = queryClient.query(query, TestRunner.getInflux2conf().getOrgId());
+
+        assertThat(tables.size()).isGreaterThan(0);
+
+        TestRunner.printTables(query, tables);
+
+        tables.forEach(table -> {
+            assertThat(table.getRecords().size()).isGreaterThan(0);
+            table.getRecords().forEach(record -> {
+                assertThat(record.getMeasurement()).isEqualTo("system");
+                assertThat(record.getField()).isEqualTo("load1");
+                assertThat(record.getValue() instanceof Double).isTrue();
+                assertThat((Double) record.getValue()).isGreaterThanOrEqualTo(0);
+            });
+        });
+
+    }
+
+    @Test
+    public void TotalMemoryTest(){
+
+        String query = String.format("dashboardTime = -15m\n" +
+                "from(bucket: \"%s\")\n" +
+                "  |> range(start: dashboardTime)\n" +
+                "  |> filter(fn: (r) => r._measurement == \"mem\" and (r._field == \"total\"))\n" +
+                "  |> map(fn: (r) => r._value/1024/1024/1024)",
+                TestRunner.getTestConf().getOrg().getBucket());
+
+        List<FluxTable> tables = queryClient.query(query, TestRunner.getInflux2conf().getOrgId());
+
+        assertThat(tables.size()).isGreaterThan(0);
+
+        TestRunner.printTables(query, tables);
+
+        tables.forEach(table -> {
+            assertThat(table.getRecords().size()).isGreaterThan(0);
+            table.getRecords().forEach(record -> {
+                assertThat(record.getMeasurement()).isEqualTo("mem");
+                assertThat(record.getField()).isEqualTo("total");
+                assertThat(record.getValue() instanceof Long).isTrue();
+                assertThat((Long) record.getValue()).isGreaterThan(0);
+            });
+        });
+
+    }
+
+    @Test
+    public void CpuUsage(){
+
+        String query = String.format("dashboardTime = -15m\n" +
+                "autoInterval = 30s\n\n" +
+                "from(bucket: \"%s\")\n" +
+                "  |> range(start: dashboardTime)\n" +
+                "  |> filter(fn: (r) => r._measurement == \"cpu\"\n" +
+                "  and \t(r._field == \"usage_system\" or r._field == \"usage_user\" or r._field == \"usage_idle\")\n  and r.host == \"%s\"\n" +
+                "  and     r.cpu == \"cpu-total\" )\n" +
+                "  |> window(every: autoInterval)\n" +
+                "  |> group(columns: [\"_field\"], mode: \"by\")",
+                TestRunner.getTestConf().getOrg().getBucket(),
+                TestRunner.getTestConf().getHostname());
+
+        System.out.println("DEBUG query: " + query);
+
+        List<FluxTable> tables = queryClient.query(query, TestRunner.getInflux2conf().getOrgId());
+
+        assertThat(tables.size()).isGreaterThan(0);
+
+        TestRunner.printTables(query, tables);
+
+        tables.forEach(table -> {
+            assertThat(table.getRecords().size()).isGreaterThan(0);
+            table.getRecords().forEach(record -> {
+                assertThat(record.getMeasurement()).isEqualTo("cpu");
+                assertThat(record.getField().equals("usage_idle") ||
+                        record.getField().equals("usage_system") ||
+                        record.getField().equals("usage_user")).isTrue();
+                assertThat(record.getValue() instanceof Double).isTrue();
+                assertThat((Double) record.getValue() >= 0.0 &&
+                        (Double) record.getValue() < 100.0).isTrue();
+            });
+        });
+
 
     }
 
