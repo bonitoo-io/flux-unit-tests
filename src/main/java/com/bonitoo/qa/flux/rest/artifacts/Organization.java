2ebccf4ded1b2a71c5117d243536ae12e6e09d18
karel-rehor <karel.k.rehor@email.cz>
2019-04-26 10:26:36 +0200

    updating schema mapping

--- src/main/java/com/bonitoo/qa/flux/rest/artifacts/Organization.java
+++ src/main/java/com/bonitoo/qa/flux/rest/artifacts/Organization.java
@@ -4,6 +4,7 @@ public class Organization {
 
     private String id;
     private String name;
+    private String description;
     private OrganizationLinks links;
 
     public String getId() {
@@ -20,6 +21,14 @@ public class Organization {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public OrganizationLinks getLinks() {
